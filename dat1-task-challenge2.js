a042e830eb1f44060138641b61a6d8a30ef99ee2
kmahendi <Krishnamoorthy.Mahendiran@csscorp.com>
2021-11-29 19:16:38 +0530

    DAY1-task-challenge2-update

--- dat1-task-challenge2.js
+++ dat1-task-challenge2.js
@@ -1,7 +1,7 @@
+const bird = 'Pidgeons';
 ( function () {
-    var bird = 'Pidgeons';
     if ( typeof bird === 'undefined' ){
-        var bird = 'Rubber Duck';
+        bird = 'Rubber Duck';
         console.log('Ernie loves his ' + bird );
     } else {
         console.log('Bert loves his ' + bird );
