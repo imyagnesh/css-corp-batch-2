534f1465245db92edc0f662f50f09c09e1434424
Akshaya <Akshaya.Ganesan@csscorp.com>
2021-11-29 14:05:30 +0530

    Day 2 task completed

--- Day2_Task.js
+++ Day2_Task.js
@@ -12,45 +12,33 @@ const users = [
 // 1. if user male then add profession: 'cricketer'
 // 2. if user is female add profession 'actor'
 
-// #Method 1
-const updateProfession = users.map(data => {
-    if(data.gender === "male") {
-        data.profession = "cricketer"
-    } else {
-        data.profession = "actor"
-    }
-})
-console.log(users);
-
-// #Method 2
-const index = users.filter(item => {
+const updateProfession = users.map(item => {
     if (item.gender === "male") {
-        item.profession = "cricketer"
+        console.log({...item, profession: "Cricketer"});
+        return ({...item, profession: "Cricketer"})
     } else {
-        item.profession = "actor"
+        console.log({...item, profession: "Actor"}); 
+        return ({...item, profession: "Actor"});
     }
 });
 
-// Day 2 Task
-
 // update the user name whoes age is > 30;
 
-const userAge = users.filter(data => {
-    if (data.age > 30) {
-        data.name = "Akshaya";
-        console.log(data.age);
-        console.log(data)
+const userNames = users.map(item => {
+    if (item.age > 30) {
+        return ({...item, name: "Akshaya"}); 
     }
 });
 
 // if male is there then Mr. name
 // if female then Ms. name
-const maleNames = users.filter(item => {
+
+const genNames = users.map(item => {
     if (item.gender === "male") {
-        item.name = "Mr." + item.name;
-        console.log(item.name)
+        console.log({...item, name: `Mr. ${item.name}`});
+        return ({...item, name: `Mr. ${item.name}`}); 
     } else {
-        item.name = "Ms." + item.name;
-        console.log(item.name);
+        console.log({...item, name: `Ms. ${item.name}`});
+        return ({...item, name: `Ms. ${item.name}`});
     }
 });
