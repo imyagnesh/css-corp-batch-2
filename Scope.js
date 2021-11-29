92de916fc76c068bbaae532a46ee2e1ba05adc59
Jeevitha <jeevithagv041997@gmail.com>
2021-11-29 22:57:16 +0530

    Day 1 Task

--- Scope.js
+++ Scope.js
@@ -1,9 +1,5 @@
-## challenge 1:
+// challenge 1:
 
-Create a file named `scope.js`.
-
-In that file, copy the following code:
-```js
 const a = 1; const b = 2; const c = 3;
 
 (function firstFunction () {
@@ -11,7 +7,8 @@ const a = 1; const b = 2; const c = 3;
 
   (function secondFunction () {
     const b = 8;
-
+    console.log(`a: ${a}, b: ${b}, c: ${c}`);
+    
     (function thirdFunction () {
       const a = 7; const c = 9;
 
@@ -20,28 +17,18 @@ const a = 1; const b = 2; const c = 3;
       })()
     })()
   })()
-})()
-```
+})();
 
-Use your knowledge of the variables' `scope` and place the following code inside one of the functions in `scope.js`
-so the output is `a: 1, b: 8, c: 6`
+// challenge 2:
 
-```js
-console.log(`a: ${a}, b: ${b}, c: ${c}`);
-```
+// Please help me and fix this code to get 'Bert loves his Pidgeons'.
 
-## challenge 2:
-
-Please help me and fix this code to get 'Bert loves his Pidgeons'.
-
-```js
-var bird = 'Pidgeons';
+let bird = 'Pidgeons';
 ( function () {
     if ( typeof bird === 'undefined' ){
-        var bird = 'Rubber Duck';
+        let bird = 'Rubber Duck';
         console.log('Ernie loves his ' + bird );
     } else {
         console.log('Bert loves his ' + bird );
     }
 }() );
-```
