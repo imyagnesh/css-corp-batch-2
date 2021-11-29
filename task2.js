67173c84e333c44f2d022a4c5e7a712a8c2e84cb
sankar <sankar.murugan@csscorp.com>
2021-11-29 12:10:11 +0530

    task 1 and task 2

--- task2.js
+++ task2.js
@@ -1,19 +1,38 @@
-// challenge 1:
-const a = 1; const b = 2; const c = 3;
+const users = [
+  { id: 1, name: "Yagnesh", gender: 'male', age: 33 },
+  { id: 2, name: "Virat", gender: 'male', age: 28 },
+  { id: 3, name: "Rohit", gender: 'male', age: 32 },
+  { id: 4, name: "Alia", gender: 'female', age: 18 },
+  { id: 5, name: "dipeeka", gender: 'female', age: 26 },
+  { id: 6, name: "Priyanka", gender: 'female', age: 38 },
+  { id: 7, name: "Taimur", gender: 'male', age: 08 },
+  { id: 8, name: "Amitabh", gender: 'male', age: 70 },
+];
 
-(function firstFunction () {
-  const b = 5; const c = 6;
+// prof
+const prof =users.map(x =>
+  {
+      if (x.gender === 'male') {
+          return {...x, profession: 'Cricketer'}
+      }
+      return {...x, profession: 'Actor'}
+  });
+console.log(prof);
 
-  (function secondFunction () {
-    const b = 8;
+//mr and ms
 
-    (function thirdFunction () {
-      const a = 7; const c = 9;
-
-      (function fourthFunction () {
-        const a = 1; const c = 8;
-      })()
-    })()
-    console.log(`a: ${a}, b: ${b}, c: ${c}`);
-  })()
-})()
+const usertype = users.map(x =>
+  {
+      if (x.age >=30) {
+          if (x.gender === 'female') {
+              return {...x, name: `Ms.  ${x.name}`}
+          }
+          else {
+              return {...x, name: `Mr. ${x.name}`}
+          }
+          } else {
+              return {...x}
+          }
+      
+  });
+console.log(usertype);
