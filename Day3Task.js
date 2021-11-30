26f0d20dc96ea4acfa11fca0bce0d05a735fe375
Akshaya <Akshaya.Ganesan@csscorp.com>
2021-11-30 20:37:38 +0530

    Day 3 Task(Sort and Reduce Method) completed

---
+++ Day3Task.js
@@ -0,0 +1,53 @@
+const users = [
+    { id: 1, name: "Yagnesh", gender: 'male', age: 33 },
+    { id: 2, name: "Virat", gender: 'male', age: 28 },
+    { id: 3, name: "Rohit", gender: 'male', age: 32 },
+    { id: 4, name: "Alia", gender: 'female', age: 18 },
+    { id: 5, name: "dipeeka", gender: 'female', age: 26 },
+    { id: 6, name: "Priyanka", gender: 'female', age: 38 },
+    { id: 7, name: "Taimur", gender: 'male', age: 08 },
+    { id: 8, name: "Amitabh", gender: 'male', age: 70 },
+    { id: 9, name: "Rajini", gender: 'male', age: 75 },
+    { id: 10, name: "Akshaya", gender: 'female', age: 24 },
+    { id: 11, name: "Siddarth", gender: 'male', age: 3 },
+    { id: 12, name: "Vijay", gender: 'male', age: 45 },
+]
+
+// Method 1 using sort
+
+// Sorting
+const sortMethod = users.sort((a, b) => a.age - b.age)
+console.log(sortMethod);
+
+// Reduce + Sort Method
+const sortByAge = sortMethod.reduce((previousValue, currentValue) => {
+    const age = Math.floor((currentValue.age / 10));
+    const key = `${age}0 - ${age}9`;
+    (previousValue[key] = previousValue[key] ?? []).push(currentValue);
+    return previousValue;
+}, {});
+
+console.log(sortByAge);
+
+// Method 2 using indexOf
+// Reduce and Sort
+const sortedByAge = users.reduce((previousValue, currentValue) => {
+    const age = Math.floor((currentValue.age / 10));
+    const ageFormat = `${age}0 - ${age}9`;
+    previousValue[ageFormat] = previousValue[ageFormat] ?? [];
+    const indexofMethod = previousValue[ageFormat].indexOf( x => {
+        return (x.age > currentValue.age)
+    });
+    if (indexofMethod > -1) {
+        previousValue[ageFormat] = [
+            ...previousValue[ageFormat].slice(0, indexofMethod),
+            currentValue,
+            ...previousValue[ageFormat].slice(indexofMethod)
+        ];
+    } else {
+        previousValue[ageFormat].push(currentValue);
+    }
+    return previousValue;
+}, {});
+
+console.log(sortedByAge);
