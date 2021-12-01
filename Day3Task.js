0dd6910b5d33305f46dd641d04d4fb97fd1e18f8
Akshaya <Akshaya.Ganesan@csscorp.com>
2021-12-01 23:00:56 +0530

    Day 3 Task Completed

--- Day3Task.js
+++ Day3Task.js
@@ -3,40 +3,25 @@ const users = [
     { id: 2, name: "Virat", gender: 'male', age: 28 },
     { id: 3, name: "Rohit", gender: 'male', age: 32 },
     { id: 4, name: "Alia", gender: 'female', age: 18 },
-    { id: 5, name: "dipeeka", gender: 'female', age: 26 },
+    { id: 5, name: "Dipeeka", gender: 'female', age: 26 },
     { id: 6, name: "Priyanka", gender: 'female', age: 38 },
     { id: 7, name: "Taimur", gender: 'male', age: 08 },
     { id: 8, name: "Amitabh", gender: 'male', age: 70 },
     { id: 9, name: "Rajini", gender: 'male', age: 75 },
     { id: 10, name: "Akshaya", gender: 'female', age: 24 },
+    { id: 10, name: "Alia", gender: 'female', age: 24 },
     { id: 11, name: "Siddarth", gender: 'male', age: 3 },
     { id: 12, name: "Vijay", gender: 'male', age: 45 },
 ]
 
-// Method 1 using sort
-
-// Sorting
-const sortMethod = users.sort((a, b) => a.age - b.age)
-console.log(sortMethod);
-
-// Reduce + Sort Method
-const sortByAge = sortMethod.reduce((previousValue, currentValue) => {
-    const age = Math.floor((currentValue.age / 10));
-    const key = `${age}0 - ${age}9`;
-    (previousValue[key] = previousValue[key] ?? []).push(currentValue);
-    return previousValue;
-}, {});
-
-console.log(sortByAge);
-
-// Method 2 using indexOf
-// Reduce and Sort
 const sortedByAge = users.reduce((previousValue, currentValue) => {
     const age = Math.floor((currentValue.age / 10));
     const ageFormat = `${age}0 - ${age}9`;
     previousValue[ageFormat] = previousValue[ageFormat] ?? [];
-    const indexofMethod = previousValue[ageFormat].indexOf( x => {
-        return (x.age > currentValue.age)
+    const indexofMethod = previousValue[ageFormat].findIndex( x => {
+        if (x.name > currentValue.name) {
+            return 1
+        }
     });
     if (indexofMethod > -1) {
         previousValue[ageFormat] = [
