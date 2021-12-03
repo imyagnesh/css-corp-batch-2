286c694f101450319a490f9a29345abe18dba779
Yagnesh Modh <yagnesh.modh@gmail.com>
2021-12-03 13:52:59 +0530

    commit code for the setup

---
+++ build/bundle.js
@@ -0,0 +1,49 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\nObject(function webpackMissingModule() { var e = new Error(\"Cannot find module './src/app'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }());\n // import { a as aa } from './app'\n\nconsole.log(Object(function webpackMissingModule() { var e = new Error(\"Cannot find module './src/app'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(1, 2));\nconsole.log(Object(function webpackMissingModule() { var e = new Error(\"Cannot find module './src/app'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));\nvar a = \"hello\"; // import * as test from './app';\n// New Javascript import syntax\n//import * as test from  './app'\n// OLD JAVASCRIPT Syntax\n// const app = require('./app');\n\nconsole.log(Object(function webpackMissingModule() { var e = new Error(\"Cannot find module './src/app'\"); e.code = 'MODULE_NOT_FOUND'; throw e; }()));\nconsole.log(\"hello\");\nconsole.log(\"how are you?\");\n\n//# sourceURL=webpack://css-corp-batch-2/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import Xyz, { a as aa, d } from './app';
+
+// import { a as aa } from './app'
+
+console.log(Xyz(1, 2));
+
+console.log(d);
+
+// import * as test from './app';
+
+// New Javascript import syntax
+// import * as test from  './app'
+
+// OLD JAVASCRIPT Syntax
+// const app = require('./app');
+
+console.log(aa);
+
+console.log('hello');
+
+console.log('how are you?');
