8aa1e161f0ec946228a57fcf325e6e1bb6b0af56
Pitchaimuthu <Pitchaimuthu.Ramesh@csscorp.com>
2021-12-06 10:42:17 +0530

    Day 3 Task Completed

---
+++ Day3-Task.js
@@ -0,0 +1,69 @@
+const users = [
+    { id: 1, name: "Yagnesh", gender: 'male', age: 33 },
+    { id: 2, name: "Virat", gender: 'male', age: 28 },
+    { id: 3, name: "Rohit", gender: 'male', age: 32 },
+    { id: 4, name: "Alia", gender: 'female', age: 18 },
+    { id: 5, name: "dipeeka", gender: 'female', age: 26 },
+    { id: 6, name: "Priyanka", gender: 'female', age: 38 },
+    { id: 7, name: "Taimur", gender: 'male', age: 08 },
+    { id: 8, name: "Amitabh", gender: 'male', age: 70 },
+]
+
+// {
+//     // "00-09": [],
+//     "10-19": [],
+//     "20-29": [],
+// }
+
+const age = 29;
+
+const ageGroup = Math.floor(age / 10);
+
+console.log(ageGroup);
+
+const key = `${ageGroup}0-${ageGroup}9`;
+
+console.log(key);
+
+
+// Reduce -> O(N)
+// sort -> O(N)
+// O(2N)
+
+// reduce + sort  -> O(N)
+
+// Sort by Age
+
+const sortbyage = users.sort((a,b)=>a.age - b.age);
+
+const groupByAge = sortbyage.reduce((previousValue, currentValue) => {
+    const ageGroup = Math.floor(currentValue.age / 10);
+    const key = `${ageGroup}0-${ageGroup}9`;
+    (previousValue[key] = previousValue[key] || []).push(currentValue)
+    return previousValue;
+}, {});
+
+console.log(groupByAge);
+
+
+// Sort by Name
+
+const sortedData = users.sort((a, b) => {
+    const nameA = a.name.toUpperCase()
+    const nameB = b.name.toUpperCase()
+    if(nameA < nameB) {
+        return -1
+    }
+    if(nameA > nameB) {
+        return 1
+    }
+});
+
+const groupByName = sortedData.reduce((previousValue, currentValue) => {
+    const ageGroup = Math.floor(currentValue.age / 10);
+    const key = `${ageGroup}0-${ageGroup}9`;
+    (previousValue[key] = previousValue[key] || []).push(currentValue)
+    return previousValue;
+}, {});
+
+console.log(groupByName);
