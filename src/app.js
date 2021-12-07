016aca84523cd263c9d88d6244279e950586a3a6
Hemant <hemant123yoga@gmail.com>
2021-12-08 01:03:41 +0530

    Javascript setup Completed

---
+++ src/app.js
@@ -0,0 +1,7 @@
+export const a = 'hello a from app';
+
+export const b = 'hello b from app';
+
+// const sum = (a,b) => a + b;
+
+// export default sum;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { a, b } from './app';
+
+// import * as test from './app';
+
+console.log('hello');
+
+console.log('how are you');
+
+console.log(a);
+
+console.log(b);
