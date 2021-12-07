9dbbaa2333e8cbfb34a18ddb9276194ec404d995
Raghavendra <rao.raghuba@gmail.com>
2021-12-07 09:01:02 +0530

    commit further set up

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -16,4 +17,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
 };
