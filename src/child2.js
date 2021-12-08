5dff6089ceee73b3d32c6204dc4d5ab6b0a16926
Shobha Rani <shobharani.baben@csscorp.com>
2021-12-08 17:19:49 +0530

    Revert "Merge branch 'react' of https://github.com/imyagnesh/css-corp-batch-2 into Day3_task_Shobha"
    This reverts commit c56812f895dd4195a6dc438d403ff30206e07e01, reversing
    changes made to 2b1e093ad47c10c28d45f6c66663846acc0ae173.

--- src/child2.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Child2 extends Component {
-  render() {
-    console.log('render child2');
-    return <div>Child 2 Component</div>;
-  }
-}
