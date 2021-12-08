bfcfc40175f4acd3526c2835bfb5862e6354cb26
Shobha Rani <shobharani.baben@csscorp.com>
2021-12-08 16:57:48 +0530

    Shobharani_reactCode_12_08

---
+++ src/app.js
@@ -0,0 +1,6 @@
+export const a = 'Hello a from app.js';
+export const b = 'Hello b from app.js';
+export const d = a ?? b;
+
+const sum = (a, b) => a + b;
+export default sum;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import Xyz from './app';
+
+console.log(Xyz(1, 2));
+
+// import { a, b } from './app'
+// new JS method
+// import * as test from './app'
+
+// OLD  JS method
+// const app= require('./app');
+// console.log(a);
+// console.log(b);
+
+console.log('hello');
+
+console.log('how are you');
+
+const y = 'hi';
+y = 'hello';
