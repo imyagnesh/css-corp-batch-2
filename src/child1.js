cd28d2ef4dd1bc89ce21fc10da9279811a5a1778
Yagnesh Modh <yagnesh.modh@gmail.com>
2021-12-09 13:55:43 +0530

    completed Updating

--- src/child1.js
+++ src/child1.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { memo } from 'react';
 
 const Child1 = () => {
-  console.log('Child1 Render');
+  console.log('Child 1 component');
   return (
     <div>
       <h1>Child 1 Component</h1>
@@ -9,4 +9,7 @@ const Child1 = () => {
   );
 };
 
-export default Child1;
+export default memo(
+  Child1,
+  (prevProps, nextProps) => prevProps.counter > nextProps.counter,
+);
