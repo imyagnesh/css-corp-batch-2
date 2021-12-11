a23265c042c17b7281f3a1e51dfa58a07a689a13
Sundarjai2 <sundar.jayadoss@gmail.com>
2021-12-12 02:15:09 +0530

    React setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+ReactDOM.render( <
+    App name = "yagnesh"
+    color = "red" / > ,
+    document.getElementById('root'),
+);
