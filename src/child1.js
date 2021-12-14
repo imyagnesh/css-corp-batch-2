9e67def532c7bfac9293960537b03a91eddf371f
Hemant <hemant123yoga@gmail.com>
2021-12-14 11:37:47 +0530

    Life cycle and Updating completed

--- src/child1.js
+++ src/child1.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { memo } from 'react';
 
-const child1 = () => {
+const Child1 = () => {
+    console.log('Child 1 component');
     return (
         <div>
-            <h1>Child 1 component</h1>
+            <h1>Child 1 Component</h1>
         </div>
-    )
-}
+    );
+};
 
-export default child1
+export default memo(
+    Child1,
+    (prevProps, nextProps) => prevProps.counter > nextProps.counter,
+);
