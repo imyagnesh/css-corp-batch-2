4863705499b4652ccdf42a6928e88dbb89445218
Medaboina Praveen <SLV131120@LAP-01--4301.ad.csscorp.com>
2021-12-15 11:48:55 +0530

    praveen-dec-14-21-task-completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './app';
-import Todo from './pages/Todo';
+//import Todo from './pages/Todo';
+import Weather from './pages/weather';
 import './root.css';
 // import UserGreet from './Input';
 
@@ -32,4 +33,5 @@ import './root.css';
 
 // Class Component
 
-ReactDOM.render(<Todo />, document.getElementById('root'));
+//ReactDOM.render(<Todo />, document.getElementById('root'));
+ReactDOM.render(<Weather />, document.getElementById('root'));
