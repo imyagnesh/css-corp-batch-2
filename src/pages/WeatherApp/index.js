eae83063264b96f2dd56eb68cc2d1ee057904368
Dineshbala Kuppan <Dineshbala.Kuppan@csscorp.com>
2021-12-16 17:36:51 +0530

    Weather App with API usage

--- src/pages/WeatherApp/index.js
+++ src/pages/WeatherApp/index.js
@@ -1,45 +1,15 @@
-import React, { Component, createRef } from 'react';
+import React, { Component, createRef, lazy, Suspense } from 'react';
 import cn from 'classnames';
 import './weatherApp.css';
+
+const WeatherForm = lazy(() => import('./weatherForm'));
+const WeatherResult = lazy(() => import('./WeatherResult'));
 
 export default class WeatherApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cityList:[
-        {
-          name: "Delhi", 
-          temperature: "28",
-        }, 
-        {
-          name: "Mumbai", 
-          temperature: "18",
-        }, 
-        {
-          name: "Kolkata", 
-          temperature: "22",
-        }, 
-        {
-          name: "Bangalore", 
-          temperature: "12",
-        }, 
-        {
-          name: "Chennai", 
-          temperature: "13",
-        }, 
-        {
-          name: "Hyderabad", 
-          temperature: "17",
-        }, 
-        {
-          name: "Pune", 
-          temperature: "18",
-        }, 
-        {
-          name: "Ahmedabad", 
-          temperature: "23",
-        }, 
-      ],
+      cityList:[],
       cityToFilter: '',
     };
 
@@ -50,52 +20,51 @@ export default class WeatherApp extends Component {
     event.preventDefault();
     this.setState(
       ({ cityToFilter }) => {
-        // const todoText = document.getElementById('todoText').value;
-        // O(1)
         const cityName = this.inputText.current.value;
         return {
           cityToFilter: cityName.toString().trim(),
         };
       },
       () => {
-        // document.getElementById('todoText').value = '';
         this.inputText.current.value = '';
         this.inputText.current.focus();
       },
     );
   };
   
+  async componentDidMount() {
+    this.loadWeatherInfo();
+  }
+
+  loadWeatherInfo = async () => {
+    try {
+      let url = 'http://localhost:3000/weather-info';
+      const res = await fetch(url);
+      const json = await res.json();
+      this.setState({
+        cityList: json,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   shouldComponentUpdate(nextProps, nextState) {
     return this.state.cityToFilter != nextState.cityToFilter;
     }
   render() {
     console.log('render called');
     const { cityList, cityToFilter } = this.state;
-    const filteredCity = cityList.find(({name}) => name.toLowerCase() === cityToFilter.toLowerCase());
+    const filteredCity = cityList.find(({city}) => city.toLowerCase() === cityToFilter.toLowerCase());
     return (
       <div className="bg-slate-200 h-screen flex flex-col">
         <h1 className="text-center my-2 text-lg font-bold">Weather App</h1>
-        <form className="flex justify-center my-2" onSubmit={this.filterCity}>
-          <input type="text" className="input" ref={this.inputText} />
-          <button type="submit" className="btn-primary">
-            Check Weather
-          </button>
-        </form>
-        <div className="flex justify-center">
-          {
-            Boolean(filteredCity) 
-              ? (
-              <div className="flex items-center m-2">
-                Temperature of {filteredCity.name} is {filteredCity.temperature}
-              </div>
-              ) : (
-                Boolean(cityToFilter) && 
-                <div className="flex items-center m-2">
-                  {cityToFilter.charAt(0).toUpperCase() + cityToFilter.slice(1)} is not found in our database.
-                </div>
-              )
-          }
-        </div>
+        <Suspense fallback={<h1>Loading...</h1>}>
+          <WeatherForm filterCity={this.filterCity} ref={this.inputText}/>
+        </Suspense>
+        <Suspense fallback={<h2>Loading results...</h2>}>
+          <WeatherResult filteredCity={filteredCity} cityToFilter={cityToFilter}/>
+        </Suspense>
       </div>
     );
   }
