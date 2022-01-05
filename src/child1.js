cff1def863374607999aa3dd43125b73fc50f3c9
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-01-05 13:55:09 +0530

    started hooks

--- src/child1.js
+++ src/child1.js
@@ -1,10 +1,36 @@
-import React, { memo } from 'react';
+import React, { memo, useEffect, useState } from 'react';
 
 const Child1 = () => {
-  console.log('Child 1 component');
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const mouseMove = () => {
+      console.log('mouse Moved...');
+    };
+
+    document.addEventListener('mousemove', mouseMove);
+
+    // component Will Unmount
+    return () => {
+      document.removeEventListener('mousemove', mouseMove);
+    };
+  }, []);
+
+  const incrementCounter = () => {
+    setCounter((val) => val + 1);
+  };
+
+  if (counter > 5) {
+    throw new Error('Counter should not greater than 5');
+  }
+
   return (
     <div>
       <h1>Child 1 Component</h1>
+      <h2>{counter}</h2>
+      <button type="button" onClick={incrementCounter}>
+        Increment Counter
+      </button>
     </div>
   );
 };
