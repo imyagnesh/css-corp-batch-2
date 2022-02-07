0f5c51632ffcfd49e402700b3b83e797d9c7e67d
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-02-07 12:06:00 +0530

    setup redux devtool

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -1,10 +1,16 @@
 import loggerMiddleware from 'middleware/logger';
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 
 const middleware = [thunk, loggerMiddleware];
 
-const store = createStore(rootReducer, applyMiddleware(...middleware));
+const composeEnhancers =
+  (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middleware)),
+);
 
 export default store;
